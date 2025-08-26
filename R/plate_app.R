f98611f24f4247fe43ad82edb8f2b109cc66f85b
OmarAshkar <o_elashkar@hotmail.com>
2025-08-26 17:11:49 -0400

    browser

--- R/plate_app.R
+++ R/plate_app.R
@@ -1462,6 +1462,6 @@ plate_app <- function() {
     })
 
   }
-  runApp(list(ui = ui, server = server, port = 12344, host = '127.0.0.1'))
+  runApp(list(ui = ui, server = server, port = 12344, host = '127.0.0.1', launch.browser = TRUE))
 }
 
